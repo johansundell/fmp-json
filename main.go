b0e09ac29943fe7733cc666b2bb284ba02cec04d
Johan Sundell <johan@pixpro.net>
2016-12-28 13:30:53 +0100

    Added more tls support

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"log/syslog"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -22,19 +23,20 @@ var routes = Routes{
 	},
 }
 
-var hostname, fmServer, sslKey, sslCert string
+var httpPortInterface, tlsPortInterface, fmServer, sslKey, sslCert string
 var router *mux.Router
 var debug, useSyslog bool
 
 const appVersionStr = "0.1"
 
 func main() {
-	flag.StringVar(&hostname, "hostname", ":8080", "The hostname that will server the files")
+	flag.StringVar(&httpPortInterface, "http", ":8080", "HTTP port and interface the server will use, format interface:port")
+	flag.StringVar(&tlsPortInterface, "tls", ":1443", "TLS port and interface the server will use, format interface:port")
 	flag.StringVar(&fmServer, "server", "http://fmh-iwp12.no-ip.info", "The filemaker server to use as host")
 	flag.BoolVar(&debug, "debug", false, "Debug requests")
 	flag.BoolVar(&useSyslog, "usesyslog", false, "Use syslog")
-	flag.StringVar(&sslCert, "ssl-cert", "", "Path to the ssl cert to use, if empty it will use http")
-	flag.StringVar(&sslKey, "ssl-key", "", "Path to the ssl key to use, if empty it will use http")
+	flag.StringVar(&sslCert, "ssl-cert", "server.crt", "Path to the ssl cert to use, if empty it will use http")
+	flag.StringVar(&sslKey, "ssl-key", "server.key", "Path to the ssl key to use, if empty it will use http")
 	flag.Parse()
 
 	if useSyslog {
@@ -48,15 +50,24 @@ func main() {
 	router = NewRouter()
 	srv := &http.Server{
 		Handler: router,
-		Addr:    hostname,
+		//Addr:    httpPortInterface,
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 90 * time.Second,
 		ReadTimeout:  90 * time.Second,
 	}
-	if sslCert != "" && sslKey != "" {
-		log.Fatal(srv.ListenAndServeTLS(sslCert, sslKey))
+	useTls := false
+	if sslCert != "" && sslKey != "" && simpleFileExist(sslCert) && simpleFileExist(sslKey) {
+		useTls = true
+		srv.Addr = tlsPortInterface
+		go func() {
+			log.Fatal(srv.ListenAndServeTLS(sslCert, sslKey))
+		}()
+	}
+	//log.Println("Could not find key or cert for ssl", sslKey, sslCert)
+	if useTls {
+		log.Fatal(http.ListenAndServe(httpPortInterface, http.HandlerFunc(redir)))
 	} else {
-		log.Println("Could not find key or cert for ssl", sslKey, sslCert)
+		srv.Addr = httpPortInterface
 		log.Fatal(srv.ListenAndServe())
 	}
 }
@@ -64,7 +75,9 @@ func main() {
 func returnJson(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	enc := json.NewEncoder(w)
-	enc.SetIndent("", "\t")
+	if debug {
+		enc.SetIndent("", "\t")
+	}
 	enc.Encode(data)
 }
 
@@ -75,4 +88,16 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 func setUrl(req filemaker.Record, r *http.Request, database, layout string) {
 	url, _ := router.Get("getRecordHandler").URL("database", database, "layout", layout, "recid", req["recid"])
 	req["recid_url"] = r.Host + url.String()
+}
+
+func redir(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://"+r.Host+r.RequestURI, http.StatusMovedPermanently)
+	//http.Redirect(w, r, "https://"+r.Host+r.RequestURI, http.StatusTemporaryRedirect)
+}
+
+func simpleFileExist(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		return false
+	}
+	return true
 }
